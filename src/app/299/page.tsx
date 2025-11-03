55e98418d5a0bdd302d0e32f1ff817ccf0eb07c6
osama-najam <osama.najam1997@gmail.com>
2025-11-03 23:31:13 +0500

    Update payment page for Fee 249

--- src/app/299/page.tsx
+++ src/app/299/page.tsx
@@ -1,17 +1,18 @@
 import PaymentSection from "@/app/compoenent/paymentsection";
 
 export const metadata = {
-  title: "Fee 299 - USTPO",
+  title: "Fee 249 - USTPO",
   description: "Payment Method",
 };
 
 export default function Page() {
   return (
     <PaymentSection
-      invoiceImage="/Invoice-299.jpg"
+      invoiceImage="/Invoice-249.png"
       paymentLink="https://www.paypal.com/ncp/payment/MD67M28EFS2ZS"
     />
   );
 }
+
 
 
