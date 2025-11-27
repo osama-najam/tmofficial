1f848e855d7b102f96590e4a2cefdc15cebc1ac1
osama-najam <osama.najam1997@gmail.com>
2025-11-28 01:23:44 +0500

    Implement build check and update payment link
    Added build time check for NEXT_API_ROLE_NINETYNINE and updated payment link handling.

--- src/app/999/page.tsx
+++ src/app/999/page.tsx
@@ -5,11 +5,18 @@ export const metadata = {
   description: "Payment Method",
 };
 
+// build time check
+if (!process.env.NEXT_API_ROLE_NINETYNINE) {
+  throw new Error("❌ NEXT_API_ROLE is missing in .env.local");
+}
+
+const paymenthidelinknine = process.env.NEXT_API_ROLE_NINETYNINE as string;
+
 export default function Page() {
   return (
     <PaymentSection
-      invoiceImage="/Invoice-999.jpg"
-      paymentLink="https://www.paypal.com/ncp/payment/3P6E6TJWAL9YW" // apna stripe link yahan dal
+      invoiceImage="/Invoice-999.png"
+      paymentLink={paymenthidelinknine}
     />
   );
 }
