d57b16dad2c33815032ec50a56952706d9ee3970
osama-najam <osama.najam1997@gmail.com>
2025-11-28 01:13:30 +0500

    Refactor payment link handling in page.tsx

--- src/app/499/page.tsx
+++ src/app/499/page.tsx
@@ -5,26 +5,13 @@ export const metadata = {
   description: "Payment Method",
 };
 
-type Paymenttype = [
-  payment : string,
-]
-
-const paymenthidelkink : Paymenttype =process.env.NEXT_API_ROLE;
+const paymenthidelink = process.env.NEXT_API_ROLE;
 
 export default function Page() {
   return (
     <PaymentSection
       invoiceImage="/Invoice-499.png"
-      paymentLink={paymenthidelkink}
+      paymentLink={paymenthidelink}
     />
   );
 }
-
-
-
-
-
-
-
-
-
