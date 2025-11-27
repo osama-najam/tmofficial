ee4fd1d36b760d5b6b9e45a00bd2400d0713334c
osama-najam <osama.najam1997@gmail.com>
2025-11-28 01:08:52 +0500

    Fix payment link assignment in page component

--- src/app/499/page.tsx
+++ src/app/499/page.tsx
@@ -5,14 +5,17 @@ export const metadata = {
   description: "Payment Method",
 };
 
+const paymenthidelkink=process.env.NEXT_API_ROLE
+
 export default function Page() {
   return (
     <PaymentSection
       invoiceImage="/Invoice-499.png"
-      paymentLink=process.env.NEXT_API_ROLE
+      paymentLink={paymenthidelkink}
     />
   );
 }
+
 
 
 
