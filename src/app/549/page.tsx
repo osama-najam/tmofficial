f02e67541d6a690badfa2ae1424c27212a06291e
Moaz-khan <160053692+Moaz-khan@users.noreply.github.com>
2026-01-21 02:10:24 +0500

    Add files via upload

---
+++ src/app/549/page.tsx
@@ -0,0 +1,15 @@
+import PaymentSection from "@/app/compoenent/paymentsection";
+
+export const metadata = {
+  title: "Fee 549 - USTPO",
+  description: "Payment Method",
+};
+
+export default function Page() {
+  return (
+    <PaymentSection
+      invoiceImage="/Invoice-999.jpg"
+      paymentLink="https://www.paypal.com/paypalme/DigiclubPH?locale.x=en_US" // apna stripe link yahan dal
+    />
+  );
+}
